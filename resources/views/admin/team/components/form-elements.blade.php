1e22dd6dec9e448384dc8af9e75f1e4ecc51063e
Victor Hugo Batista <vct.bta@gmail.com>
2020-09-25 15:35:29 -0300

    Change the 'Team integrants' field name to 'Team members'

--- resources/views/admin/team/components/form-elements.blade.php
+++ resources/views/admin/team/components/form-elements.blade.php
@@ -14,10 +14,10 @@
 
 <div class="form-group row align-items-center" :class="{'has-danger': errors.has('integrants'), 'has-success': fields.integrants && fields.integrants.valid }">
     <label for="title" class="col-form-label text-md-right" :class="isFormLocalized ? 'col-md-4' : 'col-md-2'">
-        Team integrants
+        Team members
     </label>
     <div :class="isFormLocalized ? 'col-md-4' : 'col-md-9 col-xl-8'">
-        <multiselect v-model="form.integrants" placeholder="Team integrants"
+        <multiselect v-model="form.integrants" placeholder="Team members"
     label="name" track-by="id" :options="{{ $users }}" :multiple="true" open-direction="bottom">
         </multiselect>
     </div>
