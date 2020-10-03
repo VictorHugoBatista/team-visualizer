72789abd50fa53e2748dd7476d7d982630eb6ef9
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-03 10:19:35 -0300

    Add Role field and Add / Remove buttons to repeater structure

--- resources/views/admin/components/admin-user-team-field.blade.php
+++ resources/views/admin/components/admin-user-team-field.blade.php
@@ -6,16 +6,32 @@
     <div :class="isFormLocalized ? 'col-md-4' : '{{ $colSizeField }}'">
         <admin-user-team-field :options="{{ $options }}" :values="form.{{ $vModel }}" inline-template>
             <div>
-                <div v-for="value in values">
+                <div v-for="value in values" class="mb-4">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-5">
                             <select class="form-control" value="value.id">
+                                <option value="">{{ $fieldName }}</option>
                                 <option v-for="option in options"
                                     :value="option.id" v-html="option.name"
                                     :selected="option.id === value.id">
                                 </option>
                             </select>
                         </div>
+                        <div class="col-md-5">
+                            <input type="text" class="form-control" placeholder="Role" />
+                        </div>
+                        <div class="col-md-2">
+                            <div class="btn btn-block text-danger text-center">
+                                Remove
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-end">
+                    <div class="col-md-2">
+                        <button class="btn btn-block text-success text-center">
+                            Add
+                        </button>
                     </div>
                 </div>
             </div>
