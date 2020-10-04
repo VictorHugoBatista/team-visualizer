67abb271e5ea07bfd2aee26a8fadad7ce0867bee
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-04 16:10:02 -0300

    Move the sync and get methods to AdminUsersTeam model pivot and remove BO class

--- app/Models/AdminUsersTeam.php
+++ app/Models/AdminUsersTeam.php
@@ -6,4 +6,25 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class AdminUsersTeam extends Pivot
 {
+    public static function get($modelObjectFrom, $modelClassTo)
+    {
+        return $modelObjectFrom->belongsToMany($modelClassTo)
+            ->using(self::class)
+            ->withPivot([
+                'role',
+            ]);
+    }
+
+    public static function sync($dataToSync, $modelRelationshipToSync)
+    {
+        $dataToSyncToRelateCollect = collect($dataToSync);
+        $dataToSyncToRelateWithRoles = $dataToSyncToRelateCollect->mapWithKeys(function ($data) {
+            return [
+                $data['id'] => [
+                    'role' => '',
+                ],
+            ];
+        });
+        $modelRelationshipToSync->sync(collect($dataToSyncToRelateWithRoles->toArray()));
+    }
 }
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\BO\AdminUsersTeam;
+use App\Models\AdminUsersTeam;
 use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
