bc3699d2ea4f85c49248c3ec681ed9edaa00d9ac
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-07 15:29:26 -0300

    Change 'change' event emit to watch method on repeater field

--- resources/js/admin/admin-user-team-field/Form.js
+++ resources/js/admin/admin-user-team-field/Form.js
@@ -21,21 +21,20 @@ Vue.component('admin-user-team-field', {
                 id: line.id,
                 role: line.pivot.role,
             }));
-            this.emitChangeEvent();
         },
         addLine() {
             this.lines.push({
                 id: '',
                 role: '',
             });
-            this.emitChangeEvent();
         },
         removeLine(lineNumber) {
             this.lines.splice(lineNumber, 1);
-            this.emitChangeEvent();
         },
-        emitChangeEvent() {
-            this.$emit('change', this.lines);
+    },
+    watch: {
+        lines(newLines) {
+            this.$emit('change', newLines);
         },
     },
 });
