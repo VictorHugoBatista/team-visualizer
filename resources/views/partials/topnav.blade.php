33f9bd787953219120b21c5ce0418acf5cf71aca
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-12 16:45:38 -0300

    Style topnav spacing

--- resources/views/partials/topnav.blade.php
+++ resources/views/partials/topnav.blade.php
@@ -1,13 +1,13 @@
 @if (Route::has('login'))
 <div class="topnav">
     @auth
-        <a href="{{ url('/home') }}">Home</a>
+    <a class="topnav-link" href="{{ url('/home') }}">Home</a>
     @else
-        <a href="{{ route('login') }}">Login</a>
+    <a class="topnav-link" href="{{ route('login') }}">Login</a>
 
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-        @endif
+    @if (Route::has('register'))
+    <a class="topnav-link" href="{{ route('register') }}">Register</a>
+    @endif
     @endauth
 </div>
 @endif
