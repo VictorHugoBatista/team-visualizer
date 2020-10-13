8ae87dda787a1eee21fa624a7f2cd9a5a453334d
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-13 20:25:24 -0300

    Add the font family

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Noto+Sans+JP:wght@300;400&display=swap" rel="stylesheet" />
 
         <link rel="stylesheet" href="/css/app.css" />
     </head>
