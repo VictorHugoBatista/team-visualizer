3820a9019e7c7f18ecf69b1eaf23f91f3e61cfc7
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-13 08:42:34 -0300

    Add vue-carousel to project

--- resources/js/vue.js
+++ resources/js/vue.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import VueCarousel from 'vue-carousel';
+
 import showcase from './components/Showcase.vue';
 
+Vue.use(VueCarousel);
 Vue.component('showcase', showcase);
 
 const app = new Vue({
