817c665c5576826db2ecd08ea12f00d6f5f4f850
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-19 20:03:56 -0300

    Generate role and teams per users number with Faker

--- database/seeds/AdminUsersTeamSeeder.php
+++ database/seeds/AdminUsersTeamSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Models\AdminUsers;
 use App\Models\Team;
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\Seeder;
 
 class AdminUsersTeamSeeder extends Seeder
@@ -13,15 +14,19 @@ class AdminUsersTeamSeeder extends Seeder
      */
     public function run()
     {
+        $faker = FakerFactory::create();
+
         $createdAdminUsers = factory(AdminUsers::class, 10)->create();
         $this->command->info('Admin Users created!');
-        $createdAdminUsers->each(function ($adminUser) {
+
+        $createdAdminUsers->each(function ($adminUser) use ($faker) {
             $this->command->info("Creating Teams for user {$adminUser->id}...");
-            $createdAdminUserTeams = factory(Team::class, 5)->create();
-            $createdAdminUserTeams->each(function ($team) use ($adminUser) {
+            $createdAdminUserTeams = factory(Team::class, $faker->randomDigit)->create();
+
+            $createdAdminUserTeams->each(function ($team) use ($adminUser, $faker) {
                 $this->command->info("Team {$team->id} for user {$adminUser->id} created!");
                 $adminUser->teams()->attach($team, [
-                    'role' => 'test',
+                    'role' => $faker->jobTitle,
                 ]);
             });
         });
