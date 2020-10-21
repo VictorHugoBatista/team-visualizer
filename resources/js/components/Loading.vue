27fcd04bc52c8f1c953f86906c49b5414bac619a
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-21 08:51:57 -0300

    Add loading component to app blade layout

---
+++ resources/js/components/Loading.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="loading">
+        <div class="loading-content">
+            Loading...
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Loading',
+    };
+</script>
--- resources/js/vue.js
+++ resources/js/vue.js
@@ -4,9 +4,11 @@ import VueCarousel from 'vue-carousel';
 import vuex from './vuex';
 
 import showcase from './components/Showcase.vue';
+import loading from './components/Loading.vue';
 
 Vue.use(VueCarousel);
 Vue.component('showcase', showcase);
+Vue.component('loading', loading);
 
 new Vue({
     store: vuex,
