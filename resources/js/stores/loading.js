6a1a7f979b73b977e14030c8990807302295dd33
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-21 08:43:24 -0300

    Add mutations and actions to loading module

--- resources/js/stores/loading.js
+++ resources/js/stores/loading.js
@@ -3,4 +3,21 @@ export default {
     state: {
         showLoading: false,
     },
+    mutations: {
+        setLoadingVisibility(state, {visibility}) {
+            state.showLoading = visibility;
+        },
+    },
+    actions: {
+        showLoading({commit}) {
+            commit('setLoadingVisibility', {
+                visibility: true,
+            });
+        },
+        hideLoading({commit}) {
+            commit('setLoadingVisibility', {
+                visibility: false,
+            });
+        },
+    },
 };
