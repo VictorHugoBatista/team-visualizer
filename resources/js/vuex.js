6c113ba00a3bde27a8140727b8be258155e7b844
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-21 08:36:00 -0300

    Add loading module to vuex store

--- resources/js/vuex.js
+++ resources/js/vuex.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import loading from './stores/loading';
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({});
+export default new Vuex.Store({
+    modules: {
+        loading,
+    },
+});
