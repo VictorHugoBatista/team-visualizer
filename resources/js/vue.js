bf6e985395e2ccdf075e0124ae19e3727db7a29b
Victor Hugo Batista <vct.bta@gmail.com>
2020-10-21 08:30:58 -0300

    Add vuex stores basic structure

--- resources/js/vue.js
+++ resources/js/vue.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import VueCarousel from 'vue-carousel';
 
+import vuex from './vuex';
+
 import showcase from './components/Showcase.vue';
 
 Vue.use(VueCarousel);
 Vue.component('showcase', showcase);
 
-const app = new Vue({
+new Vue({
+    store: vuex,
     el: '#app',
 });
